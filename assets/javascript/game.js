f24a92676b67168db4e6c370cab600d1c6ba754f
Ryan Weingart <rmweingart5@gmail.com>
2019-06-14 20:42:14 -0400

    moved javascript text from html page to javascript page

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//Array for all player options to choose from
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Variables for the amount of guesses remaining and the amount of wins the player has
+var guessesRemaining = 5;
+var wins = 0;
+
+//HTML display reference holder variables
+var directionsDisplay = document.getElementById("directions-text");
+var playerGuessDisplay = document.getElementById("playerGuess-text");
+var wordSpacesDisplay = document.getElementById("wordSpaces-text");
+var guessesRemainingDisplay = document.getElementById("guessesRemaining-text");
+var lettersGuessedDisplay = document.getElementById("lettersGuessed-text");
+var winsDisplay = document.getElementById("wins-text");
+
+//This function will run whenever the player presses a key
+document.onkeyup = function(event) {
+
+    var playerChoice = event.key;
+
+
+
+
+
+
+}
+
+
+
+
+//create list of words and have a random word picked?
+
+
+
+
+
+
+
+
+
+
