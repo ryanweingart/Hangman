ae3e9c9fa874e49f6a425d03bd26701f424688c3
Ryan Weingart <rmweingart5@gmail.com>
2019-06-17 18:05:31 -0400

    variable and function update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,45 @@
-//Array for all player options to choose from
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-//Variables for the amount of guesses remaining and the amount of wins the player has
-var guessesRemaining = 5;
+//Arrays and variables
+var log = console.log;
+var words = ["Hawaii", "Singapore", "Thailand", "Positano"];
+var wordSelector = "";
+var wordLetters = [];
+var spaceHolders = 0;
+var correctSpaceHolders = [];
+var incorrectLetters = [];
+var guessesRemaining = 6;
 var wins = 0;
 
-//HTML display reference holder variables
-var directionsDisplay = document.getElementById("directions-text");
-var playerGuessDisplay = document.getElementById("playerGuess-text");
-var wordSpacesDisplay = document.getElementById("wordSpaces-text");
-var guessesRemainingDisplay = document.getElementById("guessesRemaining-text");
-var lettersGuessedDisplay = document.getElementById("lettersGuessed-text");
-var winsDisplay = document.getElementById("wins-text");
+// //HTML display reference holder variables
+// var directionsDisplay = document.getElementById("directions-text");
+// var playerGuessDisplay = document.getElementById("playerGuess-text");
+// var wordSpacesDisplay = document.getElementById("wordSpaces-text");
+// var guessesRemainingDisplay = document.getElementById("guessesRemaining-text");
+// var lettersGuessedDisplay = document.getElementById("lettersGuessed-text");
+// var winsDisplay = document.getElementById("wins-text");
 
-//This function will run whenever the player presses a key
-document.onkeyup = function(event) {
+// //This function will run whenever the player presses a key
+// document.onkeyup = function(event) {
 
-    var playerChoice = event.key;
+//     var playerChoice = event.key;
 
+// }
 
+function gameStarter () {
+    wordSelector = words[Math.floor(Math.random() * words.length)];
+    wordLetters = wordSelector.split("");
+    spaceHolders = wordLetters.length;
 
+    guessesRemaining = 6;
+    incorrectLetters = [];
+    correctSpaceHolders = [];
 
-
+    log(wordSelector);
+    log(wordLetters);
+    log(spaceHolders);
 
 }
 
-
-
-
-//create list of words and have a random word picked?
+gameStarter ();
 
 
 
