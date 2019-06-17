a05e7a84d15788718fa896c550d355669ddfd892
Ryan Weingart <rmweingart5@gmail.com>
2019-06-17 18:31:31 -0400

    gamestarter function seems to be complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,10 +12,10 @@ var wins = 0;
 // //HTML display reference holder variables
 // var directionsDisplay = document.getElementById("directions-text");
 // var playerGuessDisplay = document.getElementById("playerGuess-text");
-// var wordSpacesDisplay = document.getElementById("wordSpaces-text");
-// var guessesRemainingDisplay = document.getElementById("guessesRemaining-text");
+var guessesRemainingDisplay = document.getElementById("guessesRemaining-text");
 // var lettersGuessedDisplay = document.getElementById("lettersGuessed-text");
-// var winsDisplay = document.getElementById("wins-text");
+var winsDisplay = document.getElementById("wins-text");
+var correctSpaceHoldersDisplay = document.getElementById("correctSpaceHolders-text");
 
 // //This function will run whenever the player presses a key
 // document.onkeyup = function(event) {
@@ -29,13 +29,24 @@ function gameStarter () {
     wordLetters = wordSelector.split("");
     spaceHolders = wordLetters.length;
 
+    
     guessesRemaining = 6;
     incorrectLetters = [];
     correctSpaceHolders = [];
 
+    for (var blanks=0; blanks<spaceHolders; blanks++) {
+        correctSpaceHolders.push("_");
+    }
+
+    correctSpaceHoldersDisplay.textContent = correctSpaceHolders.join(" ");
+    guessesRemainingDisplay.textContent = "Lives: " + guessesRemaining;
+    winsDisplay.textContent = "Wins: " + wins;
+    // lettersGuessedDisplay.textContent = 
+
     log(wordSelector);
     log(wordLetters);
     log(spaceHolders);
+    log(correctSpaceHolders);
 
 }
 
