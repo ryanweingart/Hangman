e9835127215af509d89d08d943a52241785fdb85
Ryan Weingart <rmweingart5@gmail.com>
2019-06-23 18:35:33 -0400

    css additions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,8 +21,6 @@ document.onkeyup = function(event) {
 }
 
 function gameStarter () {
-    //     var wordSelect = Math.floor(Math.random()*words.length);
-    // var pickRandomWord = words[wordSelect];
     wordSelector = words[Math.floor(Math.random() * words.length)];
     wordLetters = wordSelector.split("");
     wordLength = wordLetters.length;
